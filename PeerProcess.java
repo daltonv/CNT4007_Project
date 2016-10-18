5d22f1cedc37b5427f341e99df89c9d32b109388
daltonv <daltonverhagen@gmail.com>
2016-10-18 16:31:56 -0400

    started run function in peer proccess

--- PeerProcess.java
+++ PeerProcess.java
@@ -8,11 +8,17 @@ public class PeerProcess {
 	private int myID;
 
 	public PeerProcess(int myID) throws UnknownHostException, IOException {
-		
 		this.myID = myID;
 		this.config = new Config("common.cfg", "peerinfo.cfg");
 		//TODO bitfield class init
 		//TODO record class init
+	}
+
+	public void run() {
+		for (int i=0; i<config.getPeerCount(); i++) {
+			Socket = new Socket(config.getHosts(i),config.getPorts().get(i));
+
+		}
 	}
 
 }
