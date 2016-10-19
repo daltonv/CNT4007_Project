c7d5dfc1447076c204b7d6d961eb76b93083877b
reedavers <reedavers@yahoo.com>
2016-10-19 16:00:59 -0400

    uploading the message.java file
    this Message class includes message and handshake

--- Message.java
+++ Message.java
@@ -1,5 +1,8 @@
-import java.io,*;
-import java.net,*;
+import java.io.*;
+import java.net.*;
+import java.lang.*;
+import java.util.Scanner;
+
 
 public class Message{
 	final static int CHOKE = 0;
@@ -13,6 +16,7 @@ public class Message{
 
 	private int type;
 	private int length;
+	private int ID;
 	private byte[] payload;
 
 	public Message(){
@@ -21,7 +25,7 @@ public class Message{
 		payload = null;
 	}
 
-	public void read(Socket s) throws IOException{
+	public void readMessage(Socket s) throws IOException {
 		InputStream in = s.getInputStream();
 		byte[] lengthByte = new byte[4];
 		int bytesRcvd;
@@ -30,7 +34,7 @@ public class Message{
 			bytesRcvd = in.read(lengthByte, totalBytesRcvd, 4 - totalBytesRcvd);
 			totalBytesRcvd += bytesRcvd;
 		}
-		length = Conversion.BytesToInt(lengthByte);
+		length = lengthByte[0] & 0xff;
 
 		byte[] typebyte = new byte[4];
 		totalBytesRcvd = 0;
@@ -38,6 +42,69 @@ public class Message{
 			bytesRcvd = in.read(typebyte, totalBytesRcvd, 4 - totalBytesRcvd);
 			totalBytesRcvd += bytesRcvd;
 		}
-		type = Conversion.BytesToInt(typebyte);
+		type = typebyte[0] & 0xff;
+		
+		if(length > 4){
+			payload = new byte[length - 4];
+		}
+		else{
+			payload = null;
+		}
+		totalBytesRcvd = 0;
+		while(totalBytesRcvd < length - 4){
+			bytesRcvd = in.read(payload, totalBytesRcvd, length - 4 - totalBytesRcvd);
+			totalBytesRcvd += bytesRcvd;
+		}
+	}
+
+	public void sendMessage(Socket s) throws IOException {
+		if(payload == null){
+			length = 4;
+		}
+		else{
+			length = payload.length + 4;
+		}
+		OutputStream out = s.getOutputStream();
+		out.write((byte)length);
+		out.write((byte)type);
+		if(payload != null){
+			out.write(payload);
+		}
+		out.flush();
+	}
+
+	public int getType(){
+		return type;
+	}
+
+	public int setType(int type){
+		return type;
+	}
+
+	public byte[] getPayLoad(){
+		return payload;
+	}
+
+	public void setPayLoad(byte[] payload){
+		this.payload = payload;
+	}
+
+	public void setID(int ID){
+		this.ID = ID;
+	}
+
+	public void readHandShake(Socket s) throws IOException {
+		Scanner in = new Scanner(s.getInputStream());
+		ID = in.nextInt();
+	}
+
+	public void sendHandShake(Socket s) throws IOException {
+		PrintWriter out = new PrintWriter(s.getOutputStream());
+		out.println(ID);
+		out.flush();
+	}
+
+	public int getID(){
+		return ID;
 	}
 }
